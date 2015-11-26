4dbe5a4cef41d3287cd966c0ffc64b1386e15a19
Jürgen Précour <precdsh@gmx.de>
2015-11-26 09:53:31 +0100

    issue #3
    catch errors at startup

--- ctypestest.py
+++ ctypestest.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 import ctypes
 import time
+import sys
 
-lib = ctypes.cdll.LoadLibrary("/home/pi/20151118/dgt3000.so")
+lib = ctypes.cdll.LoadLibrary("/home/pi/20151124/dgt3000.so")
 
 #but=c_char()
 #time=c_char()
@@ -10,8 +11,13 @@ but=ctypes.c_byte(0)
 buttime=ctypes.c_byte(0)
 run=1
 
-lib.dgt3000Init()
-lib.dgt3000Configure()
+res = lib.dgt3000Init()
+if(res < 0):
+	sys.exit(res)
+res = lib.dgt3000Configure()
+if(res < 0):
+	sys.exit(res)
+	
 while(run):
 	if(lib.dgt3000GetButton(ctypes.pointer(but),ctypes.pointer(buttime))==1):
 		if (but.value==1):
